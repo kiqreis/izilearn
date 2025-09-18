65530fe2f6b47239dd7a94f0096cdb71c9b86248
Kaique Reis <kreis1265@gmail.com>
2025-09-18 06:13:49 -0300

    feat: initial commit of the application

---
+++ src/main/java/br/com/izilearn/izilearn_application/IzilearnApplication.java
@@ -0,0 +1,13 @@
+package br.com.izilearn.izilearn_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzilearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IzilearnApplication.class, args);
+	}
+
+}
