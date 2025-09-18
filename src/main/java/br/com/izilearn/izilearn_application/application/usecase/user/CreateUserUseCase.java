6b6a312286c2e1f7959e5146b82382531e2550ca
Kaique Reis <kreis1265@gmail.com>
2025-09-18 08:55:29 -0300

    feat: add CreateUserUseCase with user creation business logic

--- src/main/java/br/com/izilearn/izilearn_application/application/usecase/user/CreateUserUseCase.java
+++ src/main/java/br/com/izilearn/izilearn_application/application/usecase/user/CreateUserUseCase.java
@@ -1,5 +1,7 @@
 package br.com.izilearn.izilearn_application.application.usecase.user;
 
+import br.com.izilearn.izilearn_application.application.mapper.UserMapper;
+import br.com.izilearn.izilearn_application.core.domain.model.User;
 import br.com.izilearn.izilearn_application.core.domain.repository.UserRepository;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.request.CreateUserRequest;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.response.CreateUserResponse;
@@ -12,10 +14,13 @@ import org.springframework.stereotype.Component;
 public class CreateUserUseCase {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @Transactional
     public CreateUserResponse execute(CreateUserRequest request) {
+        User user = userMapper.toUser(request);
 
+        return userMapper.toCreateUserResponse(userRepository.save(user));
     }
 
 }
