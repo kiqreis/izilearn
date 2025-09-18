292e494c910c52824f2aca28cacca0c6511f1dfb
Kaique Reis <kreis1265@gmail.com>
2025-09-18 07:42:22 -0300

    refactor: apply Lombok annotations to User entity

--- src/main/java/br/com/izilearn/izilearn_application/core/domain/model/User.java
+++ src/main/java/br/com/izilearn/izilearn_application/core/domain/model/User.java
@@ -2,10 +2,18 @@ package br.com.izilearn.izilearn_application.core.domain.model;
 
 import br.com.izilearn.izilearn_application.core.domain.enums.UserStatus;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "tb_users")
 public class User {
