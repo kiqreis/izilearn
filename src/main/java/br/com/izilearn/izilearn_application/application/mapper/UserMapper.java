08f647e7e1baabe4f0bbe2554e04f29470c6988a
Kaique Reis <kreis1265@gmail.com>
2025-09-18 17:36:49 -0300

    fix: adjust UserMapper field mappings between entity and DTOs

--- src/main/java/br/com/izilearn/izilearn_application/application/mapper/UserMapper.java
+++ src/main/java/br/com/izilearn/izilearn_application/application/mapper/UserMapper.java
@@ -4,10 +4,12 @@ import br.com.izilearn.izilearn_application.core.domain.model.User;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.request.CreateUserRequest;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.response.CreateUserResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "hashPassword", source = "password")
     User toUser(CreateUserRequest createUserRequest);
 
     CreateUserResponse toCreateUserResponse(User user);
