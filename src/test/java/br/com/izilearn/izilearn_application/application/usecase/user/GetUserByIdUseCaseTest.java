7d688d77a842dd8f1d7ea7077ea0d1b80871cab1
Kaique Reis <kreis1265@gmail.com>
2025-09-23 15:03:31 -0300

    test: implement success scenario test for GetUserByIdUseCase

--- src/test/java/br/com/izilearn/izilearn_application/application/usecase/user/GetUserByIdUseCaseTest.java
+++ src/test/java/br/com/izilearn/izilearn_application/application/usecase/user/GetUserByIdUseCaseTest.java
@@ -3,7 +3,6 @@ package br.com.izilearn.izilearn_application.application.usecase.user;
 import br.com.izilearn.izilearn_application.application.mapper.UserMapper;
 import br.com.izilearn.izilearn_application.core.domain.model.User;
 import br.com.izilearn.izilearn_application.core.domain.repository.UserRepository;
-import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.request.CreateUserRequest;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.response.CreateUserResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,15 +11,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class CreateUserUseCaseTest {
+class GetUserByIdUseCaseTest {
 
     @InjectMocks
-    private CreateUserUseCase useCase;
+    private GetUserByIdUseCase useCase;
 
     @Mock
     private UserRepository userRepository;
@@ -29,36 +31,20 @@ class CreateUserUseCaseTest {
     private UserMapper userMapper;
 
     @Test
-    @DisplayName("createUser returns a CreateUserResponse when successful")
-    void createUser_ReturnsCreateUserResponse_WhenSuccessful() {
-        CreateUserRequest request = CreateUserRequest.builder()
-                .name("Jurandir")
-                .email("jurandir@email.com")
-                .password("Jur@ndir123")
-                .cellphoneNumber("11999999999")
-                .build();
-
-        User user = new User();
-
-        user.setId(1L);
-        user.setName("Jurandir");
-        user.setEmail("jurandir@email.com");
-
+    @DisplayName("getUserById returns a CreateUseResponse when successful")
+    void getUserById_ReturnsCreateUserResponse_WhenSuccessful() {
         CreateUserResponse expectedResponse = CreateUserResponse.builder()
                 .name("Jurandir")
                 .email("jurandir@email.com")
                 .build();
 
-        when(userMapper.toUser(request))
-                .thenReturn(user);
+        when(userRepository.findById(anyLong()))
+                .thenReturn(Optional.of(new User()));
 
-        when(userRepository.save(any(User.class)))
-                .thenAnswer(i -> i.getArgument(0));
-
-        when(userMapper.toCreateUserResponse(user))
+        when(userMapper.toCreateUserResponse(any(User.class)))
                 .thenReturn(expectedResponse);
 
-        CreateUserResponse result = useCase.execute(request);
+        CreateUserResponse result = useCase.execute(1L);
 
         assertThat(result)
                 .isNotNull()
