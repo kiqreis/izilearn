a115fb5a8a23d98089cd982ba43a65feb993295f
Kaique Reis <kreis1265@gmail.com>
2025-09-23 12:15:16 -0300

    feat: implement GET user by id endpoint in UserController

--- src/main/java/br/com/izilearn/izilearn_application/infrastructure/web/controller/UserController.java
+++ src/main/java/br/com/izilearn/izilearn_application/infrastructure/web/controller/UserController.java
@@ -2,24 +2,25 @@ package br.com.izilearn.izilearn_application.infrastructure.web.controller;
 
 import br.com.izilearn.izilearn_application.application.annotations.user.PostResponse;
 import br.com.izilearn.izilearn_application.application.usecase.user.CreateUserUseCase;
+import br.com.izilearn.izilearn_application.application.usecase.user.GetUserByIdUseCase;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.request.CreateUserRequest;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.response.CreateUserResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/user")
+@Tag(name = "User", description = "Endpoints for managing users")
 public class UserController {
 
     private final CreateUserUseCase createUserUseCase;
+    private final GetUserByIdUseCase getUserByIdUseCase;
 
     @PostResponse
     @PostMapping
@@ -29,6 +30,11 @@ public class UserController {
         URI location = URI.create(String.format("/user/%s", response.getEmail()));
 
         return ResponseEntity.created(location).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CreateUserResponse> getById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(getUserByIdUseCase.execute(id));
     }
 
 }
