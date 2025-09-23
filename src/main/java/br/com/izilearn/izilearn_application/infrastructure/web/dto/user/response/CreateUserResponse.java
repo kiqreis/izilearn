4bda0ecc0fc51051dfe6a4407a053c2e201ef398
Kaique Reis <kreis1265@gmail.com>
2025-09-23 01:00:31 -0300

    refactor: enhance CreateUserResponse with @Builder for testability

--- src/main/java/br/com/izilearn/izilearn_application/infrastructure/web/dto/user/response/CreateUserResponse.java
+++ src/main/java/br/com/izilearn/izilearn_application/infrastructure/web/dto/user/response/CreateUserResponse.java
@@ -1,10 +1,12 @@
 package br.com.izilearn.izilearn_application.infrastructure.web.dto.user.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class CreateUserResponse {
 
     private String name;
