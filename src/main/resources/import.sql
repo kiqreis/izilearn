e33c8b852f00c1b60f850d88e3fef7350d33a478
Kaique Reis <kreis1265@gmail.com>
2025-09-24 09:25:33 -0300

    chore: add database seed for testing facilitation

---
+++ src/main/resources/import.sql
@@ -0,0 +1,39 @@
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Jurandir', 'jurandir@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Maria', 'maria@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('João', 'joao@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Ana', 'ana@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Carlos', 'carlos@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Fernanda', 'fernanda@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Pedro', 'pedro@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Juliana', 'juliana@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Rafael', 'rafael@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Paula', 'paula@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Lucas', 'lucas@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Mariana', 'mariana@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Bruno', 'bruno@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Camila', 'camila@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Gustavo', 'gustavo@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Larissa', 'larissa@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Thiago', 'thiago@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Aline', 'aline@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Felipe', 'felipe@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
+
+INSERT INTO tb_users(name, email, hash_password, url_image, cellphone_number, created_at, user_status) VALUES ('Patrícia', 'patricia@email.com', '123567890', 'https://default-image.com/user.png', '99999999999', CURRENT_TIMESTAMP, 'PENDING');
