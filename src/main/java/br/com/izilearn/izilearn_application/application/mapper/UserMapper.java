3946cdf5d05e38fdd171c1ecf16df4eb2446393a
Kaique Reis <kreis1265@gmail.com>
2025-09-24 09:32:27 -0300

    refactor: configure UserMapper to ignore null fields with NullValuePropertyMappingStrategy

--- src/main/java/br/com/izilearn/izilearn_application/application/mapper/UserMapper.java
+++ src/main/java/br/com/izilearn/izilearn_application/application/mapper/UserMapper.java
@@ -2,21 +2,24 @@ package br.com.izilearn.izilearn_application.application.mapper;
 
 import br.com.izilearn.izilearn_application.core.domain.model.User;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.request.CreateUserRequest;
+import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.request.UpdateUserRequest;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.response.UserResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface UserMapper {
 
     @Mapping(target = "hashPassword", source = "password")
-    @Mapping(target = "urlImage", expression = "java(defaultImage(createUserRequest.getUrlImage()))")
-    User toUser(CreateUserRequest createUserRequest);
+    @Mapping(target = "urlImage", expression = "java(defaultImage(request.getUrlImage()))")
+    User toUser(CreateUserRequest request);
 
     UserResponse toCreateUserResponse(User user);
 
-    void updateFromDto(CreateUserRequest createUserRequest, @MappingTarget User user);
+    @Mapping(target = "hashPassword", source = "password")
+    void updateFromDto(UpdateUserRequest request, @MappingTarget User user);
 
     default String defaultImage(String urlImage) {
         return urlImage != null ? urlImage : "https://default-image.com/user.png";
