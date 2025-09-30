549c77f78ad7d7b739f613a8ee9e310d575cc8b3
Kaique Reis <kreis1265@gmail.com>
2025-09-29 23:53:48 -0300

    feat: add custom query method findByEmail in UserRepository

--- src/main/java/br/com/izilearn/izilearn_application/core/domain/repository/UserRepository.java
+++ src/main/java/br/com/izilearn/izilearn_application/core/domain/repository/UserRepository.java
@@ -3,5 +3,8 @@ package br.com.izilearn.izilearn_application.core.domain.repository;
 import br.com.izilearn.izilearn_application.core.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
