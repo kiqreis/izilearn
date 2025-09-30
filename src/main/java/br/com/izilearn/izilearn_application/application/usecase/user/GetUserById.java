a9940629f2e2f20c5efc2191225c666bb7170fb6
Kaique Reis <kreis1265@gmail.com>
2025-09-30 02:09:01 -0300

    refactor: update user use cases to throw custom exceptions

--- src/main/java/br/com/izilearn/izilearn_application/application/usecase/user/GetUserById.java
+++ src/main/java/br/com/izilearn/izilearn_application/application/usecase/user/GetUserById.java
@@ -1,6 +1,7 @@
 package br.com.izilearn.izilearn_application.application.usecase.user;
 
 import br.com.izilearn.izilearn_application.application.mapper.UserMapper;
+import br.com.izilearn.izilearn_application.application.usecase.user.exception.UserNotFoundException;
 import br.com.izilearn.izilearn_application.core.domain.model.User;
 import br.com.izilearn.izilearn_application.core.domain.repository.UserRepository;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.response.UserResponse;
@@ -16,7 +17,7 @@ public class GetUserById {
 
     public UserResponse execute(Long id) {
         User user = repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new UserNotFoundException("User not found by id"));
 
         return mapper.toUserResponse(user);
     }
