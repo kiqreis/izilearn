64135394ed2e70aef5e2bcae1270fc228ebcfe3d
Kaique Reis <kreis1265@gmail.com>
2025-09-29 23:54:53 -0300

    feat: add use case GetUserByEmail for user retrieval by email

---
+++ src/main/java/br/com/izilearn/izilearn_application/application/usecase/user/GetUserByEmail.java
@@ -0,0 +1,24 @@
+package br.com.izilearn.izilearn_application.application.usecase.user;
+
+import br.com.izilearn.izilearn_application.application.mapper.UserMapper;
+import br.com.izilearn.izilearn_application.core.domain.model.User;
+import br.com.izilearn.izilearn_application.core.domain.repository.UserRepository;
+import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.response.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GetUserByEmail {
+
+    private final UserRepository repository;
+    private final UserMapper mapper;
+
+    public UserResponse execute(String email) {
+        User user = repository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return mapper.toUserResponse(user);
+    }
+
+}
