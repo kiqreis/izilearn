c9a8b10c336d149d849994b75037bd75b6a545c7
Kaique Reis <kreis1265@gmail.com>
2025-10-01 14:50:57 -0300

    feat: add Teacher entity for role separation

---
+++ src/main/java/br/com/izilearn/izilearn_application/core/domain/model/Teacher.java
@@ -0,0 +1,28 @@
+package br.com.izilearn.izilearn_application.core.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_teachers")
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "profile_id")
+    private Profile profile;
+
+    @Column(nullable = false, length = 300)
+    private String description;
+
+}
