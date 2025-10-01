eed113c024e7994ae5c8f5b6e1d0c5d5bb529c84
Kaique Reis <kreis1265@gmail.com>
2025-10-01 14:43:03 -0300

    feat: create Profile entity for user profiles

---
+++ src/main/java/br/com/izilearn/izilearn_application/core/domain/model/Profile.java
@@ -0,0 +1,29 @@
+package br.com.izilearn.izilearn_application.core.domain.model;
+
+import br.com.izilearn.izilearn_application.core.domain.enums.TypeProfile;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_profiles")
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private TypeProfile typeProfile;
+
+}
