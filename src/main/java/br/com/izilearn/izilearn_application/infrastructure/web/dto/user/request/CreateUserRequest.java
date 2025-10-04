0852817af0ab665f401193b81f22b5251ad17e17
Kaique Reis <kreis1265@gmail.com>
2025-10-04 16:53:59 -0300

    feat: implement birthDate field with date validation in CreateUserRequest

--- src/main/java/br/com/izilearn/izilearn_application/infrastructure/web/dto/user/request/CreateUserRequest.java
+++ src/main/java/br/com/izilearn/izilearn_application/infrastructure/web/dto/user/request/CreateUserRequest.java
@@ -1,11 +1,11 @@
 package br.com.izilearn.izilearn_application.infrastructure.web.dto.user.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -28,6 +28,10 @@ public class CreateUserRequest {
     @Size(max = 11)
     @NotBlank(message = "Cellphone number cannot be empty")
     private String cellphoneNumber;
+
+    @Past(message = "The date of birth must be in the past")
+    @NotNull(message = "The date of birth is mandatory")
+    private LocalDate birthDate;
 
     @Size(max = 255)
     private String urlImage;
