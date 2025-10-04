f16690a7d58b6961186313f02e39fcfddf1f663a
Kaique Reis <kreis1265@gmail.com>
2025-10-04 16:57:52 -0300

    feat: add CreateProfile for profile creation

---
+++ src/main/java/br/com/izilearn/izilearn_application/application/usecase/profile/CreateProfile.java
@@ -0,0 +1,33 @@
+package br.com.izilearn.izilearn_application.application.usecase.profile;
+
+import br.com.izilearn.izilearn_application.application.mapper.ProfileMapper;
+import br.com.izilearn.izilearn_application.application.usecase.user.exception.UserNotFoundException;
+import br.com.izilearn.izilearn_application.core.domain.enums.TypeProfile;
+import br.com.izilearn.izilearn_application.core.domain.model.Profile;
+import br.com.izilearn.izilearn_application.core.domain.model.User;
+import br.com.izilearn.izilearn_application.core.domain.repository.ProfileRepository;
+import br.com.izilearn.izilearn_application.core.domain.repository.UserRepository;
+import br.com.izilearn.izilearn_application.infrastructure.web.dto.profile.request.CreateProfileRequest;
+import br.com.izilearn.izilearn_application.infrastructure.web.dto.profile.response.ProfileResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CreateProfile {
+
+    private final ProfileRepository profileRepository;
+    private final UserRepository userRepository;
+    private final ProfileMapper mapper;
+
+    public ProfileResponse execute(CreateProfileRequest request) {
+        User user = userRepository.findById(request.getId())
+                .orElseThrow(() -> new UserNotFoundException("User not found by id"));
+
+        Profile profile = mapper.toProfile(request);
+        profile.setUser(user);
+
+        return mapper.toProfileResponse(profileRepository.save(profile));
+    }
+
+}
