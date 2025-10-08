cc63894e5826fcac5e70e0921195618759705d6c
Kaique Reis <kreis1265@gmail.com>
2025-10-08 04:54:41 -0300

    fix: adjust ProfileMapper to correctly map fields to ProfileResponse

--- src/main/java/br/com/izilearn/izilearn_application/application/mapper/ProfileMapper.java
+++ src/main/java/br/com/izilearn/izilearn_application/application/mapper/ProfileMapper.java
@@ -1,5 +1,6 @@
 package br.com.izilearn.izilearn_application.application.mapper;
 
+import br.com.izilearn.izilearn_application.core.domain.enums.TypeProfile;
 import br.com.izilearn.izilearn_application.core.domain.model.Profile;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.profile.request.CreateProfileRequest;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.profile.response.ProfileResponse;
@@ -7,15 +8,25 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ProfileMapper {
 
     @Mapping(target = "id", ignore = true)
     Profile toProfile(CreateProfileRequest request);
 
-    @Mapping(target = "userResponse", source = "user")
+    @Mapping(target = "email", expression = "java(profile.getUser().getEmail())")
+    @Mapping(target = "profiles", expression = "java(getTypeProfiles(profile))")
     ProfileResponse toProfileResponse(Profile profile);
 
     void updateFromDto(CreateProfileRequest request, @MappingTarget Profile profile);
+
+    default List<TypeProfile> getTypeProfiles(Profile profile) {
+        return profile.getUser().getProfiles()
+                .stream()
+                .map(Profile::getTypeProfile)
+                .toList();
+    }
 
 }
