4a5ecd45d3ba091367c5f1248d8b3a71028878b3
Kaique Reis <kreis1265@gmail.com>
2025-10-08 04:46:45 -0300

    refactor: update ProfileResponse fields to improve response consistency

--- src/main/java/br/com/izilearn/izilearn_application/infrastructure/web/dto/profile/response/ProfileResponse.java
+++ src/main/java/br/com/izilearn/izilearn_application/infrastructure/web/dto/profile/response/ProfileResponse.java
@@ -1,17 +1,18 @@
 package br.com.izilearn.izilearn_application.infrastructure.web.dto.profile.response;
 
 import br.com.izilearn.izilearn_application.core.domain.enums.TypeProfile;
-import br.com.izilearn.izilearn_application.infrastructure.web.dto.user.response.UserResponse;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
 public class ProfileResponse {
 
-    private TypeProfile typeProfile;
-    private UserResponse userResponse;
+    private String email;
+    private List<TypeProfile> profiles;
 
 }
