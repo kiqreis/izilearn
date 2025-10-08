c7b40144db55023d782290d3df2763b56701fec5
Kaique Reis <kreis1265@gmail.com>
2025-10-08 05:08:37 -0300

    refactor: adjust ProfileController endpoints to return proper responses

--- src/main/java/br/com/izilearn/izilearn_application/infrastructure/web/controller/ProfileController.java
+++ src/main/java/br/com/izilearn/izilearn_application/infrastructure/web/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package br.com.izilearn.izilearn_application.infrastructure.web.controller;
 
 import br.com.izilearn.izilearn_application.application.usecase.profile.CreateProfile;
+import br.com.izilearn.izilearn_application.application.usecase.profile.GetAllProfiles;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.profile.request.CreateProfileRequest;
 import br.com.izilearn.izilearn_application.infrastructure.web.dto.profile.response.ProfileResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -8,10 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,11 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProfileController {
 
     private final CreateProfile createProfile;
+    private final GetAllProfiles getAllProfiles;
 
     @PostMapping
     public ResponseEntity<ProfileResponse> save(@Valid @RequestBody CreateProfileRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(createProfile.execute(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProfileResponse> getAllUserProfiles(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(getAllProfiles.execute(id));
     }
 
 }
